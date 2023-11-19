4f11513c8d83b9f883be2abe7a82853921265d28
Emanuel Pilz <emonadeo@gmail.com>
2023-11-19 17:01:41 +0100

    format code

--- services/credit/src/main.rs
+++ services/credit/src/main.rs
@@ -1,36 +1,32 @@
 mod record;
 
-use std::sync::Arc;
-use aide::{
-    axum::ApiRouter,
-    openapi::OpenApi,
-    transform::TransformOpenApi,
-};
 use aide::axum::routing::get;
-use axum::Extension;
+use aide::{axum::ApiRouter, openapi::OpenApi, transform::TransformOpenApi};
 use axum::response::Html;
+use axum::Extension;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
-    aide::gen::extract_schemas(true);
+	aide::gen::extract_schemas(true);
 
-    let mut api = OpenApi::default();
+	let mut api = OpenApi::default();
 
-    let app = ApiRouter::new()
-        .route("/", get(handler))
-        .finish_api_with(&mut api, api_docs)
-        .layer(Extension(Arc::new(api)));
+	let app = ApiRouter::new()
+		.route("/", get(handler))
+		.finish_api_with(&mut api, api_docs)
+		.layer(Extension(Arc::new(api)));
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+	axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+		.serve(app.into_make_service())
+		.await
+		.unwrap();
 }
 
 fn api_docs(api: TransformOpenApi) -> TransformOpenApi {
-    api.title("RodeIt Credit Service")
+	api.title("RodeIt Credit Service")
 }
 
-async fn handler() -> Html<& 'static str> {
-    Html("Hello, World!")
+async fn handler() -> Html<&'static str> {
+	Html("Hello, World!")
 }
--- services/credit/src/record/mod.rs
+++ services/credit/src/record/mod.rs
@@ -1,42 +1,36 @@
-use chrono::{
-    DateTime,
-    Utc
-};
+use chrono::{DateTime, Utc};
 use schemars::JsonSchema;
-use serde::{
-    Deserialize,
-    Serialize
-};
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize, Deserialize, JsonSchema)]
 pub struct Entry {
-    // ID for Database Entry
-    pub id: Uuid,
-    // UserId
-    pub userId: Uuid,
-    // Timestamp, when this information got recorded
-    pub timestamp: DateTime<Utc>,
-    // Rollercoast ID
-    pub rcId: u32
+	// ID for Database Entry
+	pub id: Uuid,
+	// UserId
+	pub userId: Uuid,
+	// Timestamp, when this information got recorded
+	pub timestamp: DateTime<Utc>,
+	// Rollercoast ID
+	pub rcId: u32,
 }
 
 impl Entry {
-    pub fn new(userId: Uuid, rcId: u32) -> Self {
-        Self {
-            id: Uuid::uuid(),
-            userId,
-            timestamp: Utc::now(),
-            rcId
-        }
-    }
+	pub fn new(userId: Uuid, rcId: u32) -> Self {
+		Self {
+			id: Uuid::uuid(),
+			userId,
+			timestamp: Utc::now(),
+			rcId,
+		}
+	}
 
-    pub fn load(id: Uuid, userId: Uuid, timestamp: DateTime<Utc>, rcId: u32) -> Self {
-        Self {
-            id,
-            userId,
-            timestamp,
-            rcId
-        }
-    }
+	pub fn load(id: Uuid, userId: Uuid, timestamp: DateTime<Utc>, rcId: u32) -> Self {
+		Self {
+			id,
+			userId,
+			timestamp,
+			rcId,
+		}
+	}
 }
