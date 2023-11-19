422a814571ea0c67965f3d033cc92d0f460cce3d
Markus <92539850+mabezi@users.noreply.github.com>
2023-11-19 14:48:32 +0100

    Add "rcId" as field for rollercoaster

--- services/credit/src/record/mod.rs
+++ services/credit/src/record/mod.rs
@@ -16,23 +16,27 @@ pub struct Entry {
     // UserId
     pub userId: Uuid,
     // Timestamp, when this information got recorded
-    pub timestamp: DateTime<Utc>
+    pub timestamp: DateTime<Utc>,
+    // Rollercoast ID
+    pub rcId: u32
 }
 
 impl Entry {
-    pub fn new(userId: Uuid) -> Self {
+    pub fn new(userId: Uuid, rcId: u32) -> Self {
         Self {
             id: Uuid::uuid(),
             userId,
             timestamp: Utc::now(),
+            rcId
         }
     }
 
-    pub fn load(id: Uuid, userId: Uuid, timestamp: DateTime<Utc>) -> Self {
+    pub fn load(id: Uuid, userId: Uuid, timestamp: DateTime<Utc>, rcId: u32) -> Self {
         Self {
             id,
             userId,
-            timestamp
+            timestamp,
+            rcId
         }
     }
 }
