910f073f0fa9a616d87340d68a781e479fd1a800
Emanuel Pilz <emonadeo@gmail.com>
2023-11-21 20:59:00 +0100

    add surrealdb & remove build script

--- services/bucket_list/src/main.rs
+++ services/bucket_list/src/main.rs
@@ -1,12 +1,18 @@
+use std::{env::current_dir, sync::Arc};
+
 use anyhow::{Ok, Result};
 use router::create_router;
+use surrealdb::{engine::local::File, Surreal};
 
-mod router;
+pub mod router;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+	let db = Surreal::new::<File>(current_dir()?.join("bucket_list.db")).await?;
+	db.use_ns("rodeit").use_db("bucket_list").await?;
+
 	axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-		.serve(create_router().into_make_service())
+		.serve(create_router(Arc::new(db)).into_make_service())
 		.await?;
 
 	return Ok(());
--- services/bucket_list/src/router.rs
+++ services/bucket_list/src/router.rs
@@ -1,26 +1,31 @@
+use std::sync::Arc;
+
 use aide::{
-	axum::{routing::get_with, ApiRouter},
+	axum::{routing::get_with, ApiRouter, IntoApiResponse},
 	transform::TransformOperation,
 };
-use axum::Json;
+use axum::{extract::State, Json};
 use schemars::JsonSchema;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use surrealdb::{Connection, Surreal};
 
-pub fn create_router() -> ApiRouter {
-	ApiRouter::new().api_route(
-		"/",
-		get_with(bucket_lists, bucket_lists_docs)
-			.post_with(create_bucket_list, create_bucket_list_docs),
-	)
+pub fn create_router(store: Arc<Surreal<impl Connection>>) -> ApiRouter {
+	ApiRouter::new()
+		.api_route(
+			"/",
+			get_with(bucket_lists, bucket_lists_docs)
+				.post_with(create_bucket_list, create_bucket_list_docs),
+		)
+		.with_state(store)
 }
 
-#[derive(Serialize, JsonSchema)]
+#[derive(Debug, Deserialize, Serialize, JsonSchema)]
 struct BucketList {
 	id: u64,
 	name: String,
 }
 
-async fn bucket_lists() {}
+async fn bucket_lists() -> impl IntoApiResponse {}
 
 fn bucket_lists_docs(operation: TransformOperation) -> TransformOperation {
 	operation
@@ -30,7 +35,25 @@ fn bucket_lists_docs(operation: TransformOperation) -> TransformOperation {
 		})
 }
 
-async fn create_bucket_list() {}
+#[derive(Debug, Deserialize, Serialize, JsonSchema)]
+struct CreateBucketList {
+	name: String,
+}
+
+async fn create_bucket_list(
+	State(store): State<Arc<Surreal<impl Connection>>>,
+	Json(payload): Json<CreateBucketList>,
+) -> Result<(), axum::http::StatusCode> {
+	let created: Vec<CreateBucketList> = store
+		.create("bucket_list")
+		.content(CreateBucketList { name: payload.name })
+		.await
+		.or(Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR))?;
+
+	dbg!(created);
+
+	Ok(())
+}
 
 fn create_bucket_list_docs(operation: TransformOperation) -> TransformOperation {
 	operation
