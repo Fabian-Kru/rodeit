1f50ea57ced45fbf248e8e6efa62f8a4644eb70d
Emanuel Pilz <emonadeo@gmail.com>
2023-11-26 23:30:52 +0100

    add captain coaster api & dummy endpoint

--- services/bucket_list/src/main.rs
+++ services/bucket_list/src/main.rs
@@ -2,17 +2,53 @@ use std::{env::current_dir, sync::Arc};
 
 use anyhow::{Ok, Result};
 use router::create_router;
-use surrealdb::{engine::local::File, Surreal};
+use surrealdb::Surreal;
 
 pub mod router;
 
+pub struct AppState {
+	store: Surreal<surrealdb::engine::local::Db>,
+	captain_coaster_config: captain_coaster::apis::configuration::Configuration,
+}
+
+const DATABASE_FILENAME: &str = "bucket_list.db";
+const DATABASE_NAMESPACE: &str = "rodeit";
+const DATABASE_NAME: &str = "bucket_list";
+
 #[tokio::main]
 async fn main() -> Result<()> {
-	let db = Surreal::new::<File>(current_dir()?.join("bucket_list.db")).await?;
-	db.use_ns("rodeit").use_db("bucket_list").await?;
+	let db = Surreal::new::<surrealdb::engine::local::File>(current_dir()?.join(DATABASE_FILENAME))
+		.await?;
+	db.use_ns(DATABASE_NAMESPACE).use_db(DATABASE_NAME).await?;
+
+	let mut captain_coaster_default_headers = reqwest::header::HeaderMap::new();
+	captain_coaster_default_headers.insert(
+		"Accept",
+		reqwest::header::HeaderValue::from_static("application/json"),
+	);
+
+	let captain_coaster_config = captain_coaster::apis::configuration::Configuration {
+		client: reqwest::ClientBuilder::new()
+			.default_headers(captain_coaster_default_headers)
+			.build()?,
+		base_path: "https://captaincoaster.com".into(),
+		user_agent: Some("HTTPie/3.2.2".into()),
+		basic_auth: None,
+		oauth_access_token: None,
+		bearer_access_token: None,
+		api_key: Some(captain_coaster::apis::configuration::ApiKey {
+			prefix: Some("Bearer".into()),
+			key: ("ee2d7ceb-eabe-4583-8b58-be70ff1ab79e".into()),
+		}),
+	};
+
+	let app_state = Arc::new(AppState {
+		store: db,
+		captain_coaster_config,
+	});
 
 	axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-		.serve(create_router(Arc::new(db)).into_make_service())
+		.serve(create_router(app_state).into_make_service())
 		.await?;
 
 	return Ok(());
--- services/bucket_list/src/router.rs
+++ services/bucket_list/src/router.rs
@@ -1,62 +1,179 @@
 use std::sync::Arc;
 
 use aide::{
-	axum::{routing::get_with, ApiRouter, IntoApiResponse},
+	axum::{
+		routing::{get_with, post_with},
+		ApiRouter,
+	},
 	transform::TransformOperation,
 };
-use axum::{extract::State, Json};
+use axum::{
+	extract::{Path, State},
+	Json,
+};
+use captain_coaster::apis::coaster_api;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use surrealdb::{Connection, Surreal};
+use serde_with::skip_serializing_none;
 
-pub fn create_router(store: Arc<Surreal<impl Connection>>) -> ApiRouter {
-	ApiRouter::new()
-		.api_route(
-			"/",
-			get_with(bucket_lists, bucket_lists_docs)
-				.post_with(create_bucket_list, create_bucket_list_docs),
-		)
-		.with_state(store)
-}
+use crate::AppState;
 
+#[skip_serializing_none]
 #[derive(Debug, Deserialize, Serialize, JsonSchema)]
 struct BucketList {
+	user_id: u64,
+	coaster_ids: Vec<u64>,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Serialize, JsonSchema)]
+struct Park {
+	id: u64,
+	name: String,
+	country: String,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Serialize, JsonSchema)]
+struct Manufacturer {
 	id: u64,
 	name: String,
 }
 
-async fn bucket_lists() -> impl IntoApiResponse {}
-
-fn bucket_lists_docs(operation: TransformOperation) -> TransformOperation {
-	operation
-		.description("Find all bucket lists")
-		.response_with::<200, Json<Vec<BucketList>>, _>(|res| {
-			res.description("List of bucket lists")
-		})
-}
-
-#[derive(Debug, Deserialize, Serialize, JsonSchema)]
-struct CreateBucketList {
+#[skip_serializing_none]
+#[derive(Clone, Serialize, JsonSchema)]
+struct Coaster {
+	id: i32,
 	name: String,
+	speed: Option<i32>,
+	height: Option<i32>,
+	inversions: Option<i32>,
+	manufacturer: Option<Manufacturer>,
+	park: Option<Park>,
+	image: Option<String>,
 }
 
-async fn create_bucket_list(
-	State(store): State<Arc<Surreal<impl Connection>>>,
-	Json(payload): Json<CreateBucketList>,
+pub fn create_router(state: Arc<AppState>) -> ApiRouter {
+	ApiRouter::new()
+		.api_route(
+			"/:user_id",
+			get_with(find, find_docs)
+				.post_with(append, append_docs)
+				.put_with(set, set_docs),
+		)
+		.api_route(
+			"/:user_id/:index",
+			post_with(insert, insert_docs).delete_with(delete, delete_docs),
+		)
+		.with_state(state)
+}
+
+async fn find(State(state): State<Arc<AppState>>, Path(user_id): Path<u64>) -> Json<Vec<Coaster>> {
+	let coasters = coaster_api::get_coaster_collection(
+		&state.captain_coaster_config,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+		None,
+	)
+	.await
+	.unwrap_or_else(|res| {
+		dbg!(res);
+		return vec![];
+	});
+
+	let n = coasters
+		.iter()
+		.map(|coaster| Coaster {
+			id: coaster.id.unwrap_or(0),
+			name: coaster.name.clone(),
+			speed: None,
+			height: None,
+			inversions: None,
+			manufacturer: None,
+			park: None,
+			image: None,
+		})
+		.collect::<Vec<Coaster>>()
+		.to_vec();
+
+	return Json(n);
+}
+
+fn find_docs(operation: TransformOperation) -> TransformOperation {
+	operation
+		.description("Get a bucket list")
+		.response_with::<200, Json<Vec<BucketList>>, _>(|res| res.description("List of Coasters"))
+}
+
+async fn append(
+	State(store): State<Arc<AppState>>,
+	Path(user_id): Path<u64>,
+	Json(payload): Json<BucketList>,
 ) -> Result<(), axum::http::StatusCode> {
-	let created: Vec<CreateBucketList> = store
-		.create("bucket_list")
-		.content(CreateBucketList { name: payload.name })
-		.await
-		.or(Err(axum::http::StatusCode::INTERNAL_SERVER_ERROR))?;
-
-	dbg!(created);
-
-	Ok(())
+	todo!()
 }
 
-fn create_bucket_list_docs(operation: TransformOperation) -> TransformOperation {
+fn append_docs(operation: TransformOperation) -> TransformOperation {
 	operation
 		.description("Create a new bucket list")
 		.response_with::<201, (), _>(|res| res.description("Created bucket list"))
+}
+
+async fn insert(
+	State(store): State<Arc<AppState>>,
+	Path(user_id): Path<u64>,
+	Path(index): Path<u8>,
+	Json(payload): Json<BucketList>,
+) -> Result<(), axum::http::StatusCode> {
+	todo!()
+}
+
+fn insert_docs(operation: TransformOperation) -> TransformOperation {
+	operation
+		.description("Insert a bucket list")
+		.response_with::<200, (), _>(|res| res.description("Inserted bucket list"))
+}
+
+async fn set(
+	State(store): State<Arc<AppState>>,
+	Path(user_id): Path<u64>,
+	Json(payload): Json<BucketList>,
+) -> Result<(), axum::http::StatusCode> {
+	todo!()
+}
+
+fn set_docs(operation: TransformOperation) -> TransformOperation {
+	operation
+		.description("Set a bucket list")
+		.response_with::<200, (), _>(|res| res.description("Updated bucket list"))
+}
+
+async fn delete(
+	State(store): State<Arc<AppState>>,
+	Path(user_id): Path<u64>,
+	Path(index): Path<u8>,
+) -> Result<(), axum::http::StatusCode> {
+	todo!()
+}
+
+fn delete_docs(operation: TransformOperation) -> TransformOperation {
+	operation
+		.description("Delete a bucket list")
+		.response_with::<200, (), _>(|res| res.description("Deleted bucket list"))
 }
