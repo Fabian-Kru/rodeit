99346c10dae173d463f35b23ec085134bf36034c
Markus <92539850+mabezi@users.noreply.github.com>
2023-12-11 16:24:44 +0100

    rename get -> getById & add get for user_id

--- services/credit/credit.py
+++ services/credit/credit.py
@@ -11,8 +11,16 @@ def get_all():
     return credit_schema.dump(c)
 
 
-def get(credit_id: int):
+def get_by_id(credit_id: int):
     res = Credit.query.filter(Credit.id == credit_id).one_or_none()
+    if res is not None:
+        return credit_schema.dump(res)
+    else:
+        abort(404, f"Not found")
+
+
+def get(user_id: int):
+    res = Credit.query.filter(Credit.user_id == user_id).one_or_none()
     if res is not None:
         return credit_schema.dump(res)
     else:
