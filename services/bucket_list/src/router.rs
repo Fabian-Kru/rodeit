c499616ae36cf1d5644842f6df990277ec0ade9c
Emanuel Pilz <emonadeo@gmail.com>
2023-12-12 01:37:07 +0100

    bucket_list: implement find_coaster_by_user_id and add_coasters

--- services/bucket_list/src/router.rs
+++ services/bucket_list/src/router.rs
@@ -7,28 +7,32 @@ use aide::{
 	},
 	transform::TransformOperation,
 };
+use anyhow::Result;
 use axum::{
 	extract::{Path, State},
+	http::StatusCode,
 	Json,
 };
 use captain_coaster::apis::coaster_api;
+use futures::future::join_all;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
+use surrealdb::opt::PatchOp;
 
 use crate::AppState;
 
 #[skip_serializing_none]
 #[derive(Debug, Deserialize, Serialize, JsonSchema)]
 struct BucketList {
-	user_id: u64,
-	coaster_ids: Vec<u64>,
+	user_id: u32,
+	coaster_ids: Vec<u32>,
 }
 
 #[skip_serializing_none]
 #[derive(Clone, Serialize, JsonSchema)]
 struct Park {
-	id: u64,
+	id: u32,
 	name: String,
 	country: String,
 }
@@ -36,18 +40,18 @@ struct Park {
 #[skip_serializing_none]
 #[derive(Clone, Serialize, JsonSchema)]
 struct Manufacturer {
-	id: u64,
+	id: u32,
 	name: String,
 }
 
 #[skip_serializing_none]
 #[derive(Clone, Serialize, JsonSchema)]
 struct Coaster {
-	id: i32,
+	id: u32,
 	name: String,
-	speed: Option<i32>,
-	height: Option<i32>,
-	inversions: Option<i32>,
+	speed: Option<u32>,
+	height: Option<u32>,
+	inversions: Option<u32>,
 	manufacturer: Option<Manufacturer>,
 	park: Option<Park>,
 	image: Option<String>,
@@ -57,122 +61,137 @@ pub fn create_router(state: Arc<AppState>) -> ApiRouter {
 	ApiRouter::new()
 		.api_route(
 			"/:user_id",
-			get_with(find, find_docs)
-				.post_with(append, append_docs)
-				.put_with(set, set_docs),
+			get_with(find_coasters_by_user_id, docs_find_coasters_by_user_id)
+				.post_with(add_coaster, docs_add_coaster)
+				.put_with(set_coasters, docs_set_coasters),
 		)
 		.api_route(
 			"/:user_id/:index",
-			post_with(insert, insert_docs).delete_with(delete, delete_docs),
+			post_with(insert_coaster, docs_insert_coaster)
+				.delete_with(remove_coaster, docs_remove_coaster),
 		)
 		.with_state(state)
 }
 
-async fn find(State(state): State<Arc<AppState>>, Path(user_id): Path<u64>) -> Json<Vec<Coaster>> {
-	let coasters = coaster_api::get_coaster_collection(
-		&state.captain_coaster_config,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-		None,
-	)
-	.await
-	.unwrap_or_else(|res| {
-		dbg!(res);
-		return vec![];
+async fn find_coasters_by_user_id(
+	State(state): State<Arc<AppState>>,
+	Path(user_id): Path<u64>,
+) -> Result<Json<Vec<Coaster>>, StatusCode> {
+	let store = &state.store;
+
+	#[derive(Deserialize)]
+	struct SelectBucketList {
+		coaster_ids: Vec<u32>,
+	}
+
+	let bucket_list: SelectBucketList = store
+		.select(("bucket_list", user_id))
+		.await
+		.or(Err(StatusCode::INTERNAL_SERVER_ERROR))?
+		.ok_or(StatusCode::NOT_FOUND)?;
+
+	let coasters = bucket_list.coaster_ids.iter().map(|coaster_id| async {
+		let raw_coaster =
+			coaster_api::get_coaster_item(&state.captain_coaster_config, &coaster_id.to_string())
+				.await
+				.unwrap();
+
+		return Coaster {
+			id: *coaster_id,
+			name: raw_coaster.name,
+			speed: raw_coaster.speed.and_then(|speed| Some(speed as u32)),
+			height: raw_coaster.height.and_then(|height| Some(height as u32)),
+			inversions: raw_coaster
+				.inversions_number
+				.and_then(|inversions| Some(inversions as u32)),
+			manufacturer: raw_coaster.manufacturer.map(|manufacturer| Manufacturer {
+				id: 0u32, // TODO: get manufacturer id
+				name: manufacturer.name.unwrap(),
+			}),
+			park: raw_coaster.park.map(|park| Park {
+				id: 0u32, // TODO: get manufacturer id
+				name: park.name.unwrap(),
+				country: park.country.unwrap().name.unwrap(), // TODO: unwrap city
+			}),
+			image: raw_coaster.main_image.unwrap().path,
+		};
 	});
 
-	let n = coasters
-		.iter()
-		.map(|coaster| Coaster {
-			id: coaster.id.unwrap_or(0),
-			name: coaster.name.clone(),
-			speed: None,
-			height: None,
-			inversions: None,
-			manufacturer: None,
-			park: None,
-			image: None,
-		})
-		.collect::<Vec<Coaster>>()
-		.to_vec();
-
-	return Json(n);
+	return Ok(Json(join_all(coasters).await));
 }
 
-fn find_docs(operation: TransformOperation) -> TransformOperation {
+fn docs_find_coasters_by_user_id(operation: TransformOperation) -> TransformOperation {
 	operation
 		.description("Get a bucket list")
 		.response_with::<200, Json<Vec<BucketList>>, _>(|res| res.description("List of Coasters"))
 }
 
-async fn append(
+async fn add_coaster(
 	State(store): State<Arc<AppState>>,
 	Path(user_id): Path<u64>,
-	Json(payload): Json<BucketList>,
-) -> Result<(), axum::http::StatusCode> {
-	todo!()
+	Json(coaster_id): Json<u32>,
+) -> Result<Json<Vec<u32>>, StatusCode> {
+	let store = &store.store;
+
+	#[derive(Deserialize)]
+	struct UpdatedBucketList {
+		coaster_ids: Vec<u32>,
+	}
+
+	let updated: UpdatedBucketList = store
+		.update(("bucket_list", user_id))
+		.patch(PatchOp::add("/coaster_ids", &[coaster_id]))
+		.await
+		.or(Err(StatusCode::INTERNAL_SERVER_ERROR))?
+		.ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
+
+	return Ok(Json(updated.coaster_ids));
 }
 
-fn append_docs(operation: TransformOperation) -> TransformOperation {
+fn docs_add_coaster(operation: TransformOperation) -> TransformOperation {
 	operation
-		.description("Create a new bucket list")
+		.description("Add a coaster to a bucket list")
 		.response_with::<201, (), _>(|res| res.description("Created bucket list"))
 }
 
-async fn insert(
+async fn insert_coaster(
 	State(store): State<Arc<AppState>>,
-	Path(user_id): Path<u64>,
-	Path(index): Path<u8>,
+	Path(user_id): Path<u32>,
+	Path(index): Path<u32>,
 	Json(payload): Json<BucketList>,
 ) -> Result<(), axum::http::StatusCode> {
 	todo!()
 }
 
-fn insert_docs(operation: TransformOperation) -> TransformOperation {
+fn docs_insert_coaster(operation: TransformOperation) -> TransformOperation {
 	operation
 		.description("Insert a bucket list")
 		.response_with::<200, (), _>(|res| res.description("Inserted bucket list"))
 }
 
-async fn set(
+async fn set_coasters(
 	State(store): State<Arc<AppState>>,
-	Path(user_id): Path<u64>,
-	Json(payload): Json<BucketList>,
+	Path(user_id): Path<u32>,
+	Json(payload): Json<Vec<u32>>,
 ) -> Result<(), axum::http::StatusCode> {
 	todo!()
 }
 
-fn set_docs(operation: TransformOperation) -> TransformOperation {
+fn docs_set_coasters(operation: TransformOperation) -> TransformOperation {
 	operation
 		.description("Set a bucket list")
 		.response_with::<200, (), _>(|res| res.description("Updated bucket list"))
 }
 
-async fn delete(
+async fn remove_coaster(
 	State(store): State<Arc<AppState>>,
-	Path(user_id): Path<u64>,
-	Path(index): Path<u8>,
+	Path(user_id): Path<u32>,
+	Path(index): Path<u32>,
 ) -> Result<(), axum::http::StatusCode> {
 	todo!()
 }
 
-fn delete_docs(operation: TransformOperation) -> TransformOperation {
+fn docs_remove_coaster(operation: TransformOperation) -> TransformOperation {
 	operation
 		.description("Delete a bucket list")
 		.response_with::<200, (), _>(|res| res.description("Deleted bucket list"))
