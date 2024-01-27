c2659e5a522aba8dcab20b957f4e5f769d648601
Emanuel Pilz <emonadeo@gmail.com>
2024-01-27 13:59:37 +0100

    bucket_list: serialize country as country code

--- services/bucket_list/src/model/country.rs
+++ services/bucket_list/src/model/country.rs
@@ -5,55 +5,105 @@ use serde::Serialize;
 
 #[derive(Clone, Serialize, JsonSchema)]
 pub enum Country {
+	#[serde(rename = "AR")]
 	Argentina,
+	#[serde(rename = "AU")]
 	Australia,
+	#[serde(rename = "AT")]
 	Austria,
+	#[serde(rename = "BE")]
 	Belgium,
+	#[serde(rename = "BR")]
 	Brazil,
+	#[serde(rename = "MM")]
 	Burma,
+	#[serde(rename = "CA")]
 	Canada,
+	#[serde(rename = "CN")]
 	China,
+	#[serde(rename = "CO")]
 	Colombia,
+	#[serde(rename = "CY")]
 	Cyprus,
+	#[serde(rename = "CZ")]
 	CzechRepublic,
+	#[serde(rename = "DK")]
 	Denmark,
+	#[serde(rename = "FI")]
 	Finland,
+	#[serde(rename = "FR")]
 	France,
+	#[serde(rename = "DE")]
 	Germany,
+	#[serde(rename = "GT")]
 	Guatemala,
+	#[serde(rename = "HU")]
 	Hungary,
+	#[serde(rename = "IN")]
 	India,
+	#[serde(rename = "ID")]
 	Indonesia,
+	#[serde(rename = "IQ")]
 	Iraq,
+	#[serde(rename = "IE")]
 	Ireland,
+	#[serde(rename = "IL")]
 	Israel,
+	#[serde(rename = "IT")]
 	Italy,
+	#[serde(rename = "JP")]
 	Japan,
+	#[serde(rename = "LB")]
 	Lebanon,
+	#[serde(rename = "MY")]
 	Malaysia,
+	#[serde(rename = "MX")]
 	Mexico,
+	#[serde(rename = "MN")]
 	Mongolia,
+	#[serde(rename = "NL")]
 	Netherlands,
+	#[serde(rename = "NZ")]
 	NewZealand,
+	#[serde(rename = "NO")]
 	Norway,
+	#[serde(rename = "PE")]
 	Peru,
+	#[serde(rename = "PL")]
 	Poland,
+	#[serde(rename = "PT")]
 	Portugal,
+	#[serde(rename = "QA")]
 	Qatar,
+	#[serde(rename = "RU")]
 	Russia,
+	#[serde(rename = "SG")]
 	Singapore,
+	#[serde(rename = "ZA")]
 	SouthAfrica,
+	#[serde(rename = "KR")]
 	SouthKorea,
+	#[serde(rename = "ES")]
 	Spain,
+	#[serde(rename = "SE")]
 	Sweden,
+	#[serde(rename = "CH")]
 	Switzerland,
+	#[serde(rename = "TW")]
 	Taiwan,
+	#[serde(rename = "TH")]
 	Thailand,
+	#[serde(rename = "TR")]
 	Turkey,
+	#[serde(rename = "UA")]
 	Ukraine,
+	#[serde(rename = "AE")]
 	UnitedArabEmirates,
+	#[serde(rename = "GB")]
 	UnitedKingdom,
+	#[serde(rename = "US")]
 	UnitedStates,
+	#[serde(rename = "VN")]
 	Vietnam,
 }
 
