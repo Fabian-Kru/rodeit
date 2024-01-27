ab555cf305338019d0c9897b763c3accdf044a26
Emanuel Pilz <emonadeo@gmail.com>
2024-01-27 22:24:37 +0100

    web: use env var for api

--- web/src/api/mod.ts
+++ web/src/api/mod.ts
@@ -3,7 +3,7 @@ import createClient from 'openapi-fetch';
 import { paths as Paths } from './types.ts';
 
 export const client = createClient<Paths>({
-	baseUrl: 'http://0.0.0.0:3000/',
+	baseUrl: import.meta.env.VITE_RODEIT_API_URL,
 });
 
 export const picturesBaseUrl = 'https://pictures.captaincoaster.com/1440x1440/';
--- web/src/env.d.ts
+++ web/src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="@solidjs/start/env" />
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+interface ImportMetaEnv {
+	readonly VITE_RODEIT_API_URL: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
