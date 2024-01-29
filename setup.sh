b935fbf4458bc144c782247d1454a4e8fa4ca27b
Fabian Krusch <fabian.krusch@mailbox.tu-dresden.de>
2024-01-29 02:30:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
-cd ./services/bucket_list
-docker build -t bucket_list:latest .
-cd ../services/user
-docker built -t user:latest .
-cd ../services/records
-docker built -t records:latest .
+docker build -t gcr.hrz.tu-chemnitz.de/praktikum-scc/ws23-gruppe1/gruppe1/bucket_list:0.2.0 -f ./services/bucket_list/Dockerfile .
+docker build -t gcr.hrz.tu-chemnitz.de/praktikum-scc/ws23-gruppe1/gruppe1/user:0.1.0 -f ./services/user/Dockerfile .
+docker build -t gcr.hrz.tu-chemnitz.de/praktikum-scc/ws23-gruppe1/gruppe1/records:v1.f.a.5 -f ./services/records/Dockerfile .
